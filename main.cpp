0a43cf18a081b646c97df1969f72df06ae4d4ffd
ReykoPats <maurosep518@gmail.com>
2020-03-31 02:15:32 +0000

    Hice los comentarios del codigo, y de paso cargue la matriz y los valores, pues eso lo hice yo

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
+  /*
+  Menu principal. Aqui se inicia un objeto de la clase tablero. Luego, se inicializan los datos que estan en tablero.txt debajo de la matriz, se inicializan los avatares de los ejercitos y se carga el menu de juego
+  */
   tablero hacer;
   hacer.set_datos();
   hacer.set_avatares();
